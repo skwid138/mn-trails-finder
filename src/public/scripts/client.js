b671415843b15a80f29ef968f59cda6fc5249156
Hunter Rancourt <skwid138@gmail.com>
2017-10-16 09:48:37 -0500

    updated client.js config

--- src/public/scripts/client.js
+++ src/public/scripts/client.js
@@ -2,11 +2,11 @@ console.log('client.js sourced');
 // Setup Angular and source in modules
 var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial']);
 
-myApp.config(function ($routeProvider, $locationProvider, $mdThemingProvider) {
+myApp.config(function ($routeProvider) {
 
 /// Client Side Routes ///
 $routeProvider.when('/', {
-    templateUrl: '/views/home.html',
-    controller: 'HomeController as hc'
+    templateUrl: '/views/', // not a thing
+    controller: '' // not a thing
 }).otherwise('/'); // end routeProvider
 }); // end config
