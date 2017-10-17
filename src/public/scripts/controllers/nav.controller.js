82516dfea1ac202c3ac463a11248e8d7ef05b832
Hunter Rancourt <skwid138@gmail.com>
2017-10-17 16:42:16 -0500

    added inputs and buttons for user login to index, added login function, created register view file

--- src/public/scripts/controllers/nav.controller.js
+++ src/public/scripts/controllers/nav.controller.js
@@ -7,7 +7,39 @@ This controller is for the nav bar and user login
 - displays My Trails button
 */
 myApp.controller('NavController', (UserService) => {
-console.log('in NavController');
-const vm = this;
+    console.log('in NavController');
+    const vm = this;
+
+    // user object for login
+    vm.user = {
+        username: '',
+        password: ''
+    };
+
+    // message string for user login
+    vm.message = '';
+
+    // handles user login
+    vm.login = () => {
+        console.log('in login');
+        
+        if (vm.user.username === '' || vm.user.password === '') {
+            vm.message = "Enter your username and password!";
+        } else {
+            console.log('login - sending to server ', vm.user);
+            $http.post('/', vm.user).then((response) => {
+                if (response.data.username) {
+                    console.log('login success: ', response.data);
+                } else {
+                    console.log('login post failure: ', response);
+                    vm.message = "Incorrect Credentials, please try again";
+                } // end else
+            }).catch((response) => {
+                console.log('login catch - failure: ', response);
+                vm.message = "Incorrect Credentials, please try again";
+            }); // end catch
+        } // end else
+    }; // end login
+
 
 }); // end NavController
