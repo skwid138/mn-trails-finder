02aca6fc3d7bf3e47fce45ecd4262c36b53c42b0
Hunter Rancourt <skwid138@gmail.com>
2017-10-18 11:57:14 -0500

    created add and admin controllers and views

---
+++ src/public/scripts/controllers/add-trail.controller.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+
+/*
+This controller allows users to add trails to the DB
+- it will allow users to create trail records and write them to the DB
+*/
+myApp.controller('AddTrailController', function () {
+
+}); // end AddTrailController
---
+++ src/public/scripts/controllers/admin.controller.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+
+/*
+This controller is for admin users
+- it will allow users with the admin rights to approve or edit trails
+*/
+myApp.controller('AdminController', function() {
+
+}); // end AdminController
